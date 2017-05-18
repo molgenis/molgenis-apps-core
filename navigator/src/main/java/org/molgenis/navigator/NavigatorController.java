f64be3107aa8ef06d91d2ab2e5e67d08f2b7d5b5
connorstroomberg <c.stroomberg@gmail.com>
2017-05-18 13:35:59 +0200

    Setup for basic controller test

--- navigator/src/main/java/org/molgenis/navigator/NavigatorController.java
+++ navigator/src/main/java/org/molgenis/navigator/NavigatorController.java
@@ -1,14 +1,9 @@
 package org.molgenis.navigator;
 
-import org.molgenis.framework.ui.MolgenisPluginFactory;
 import org.molgenis.ui.MolgenisPluginController;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import javax.servlet.http.HttpServletRequest;
 
 import static org.molgenis.navigator.NavigatorController.URI;
 
@@ -18,15 +13,14 @@ public class NavigatorController extends MolgenisPluginController
 {
 	public static final String URI = PLUGIN_URI_PREFIX + "navigator";
 
-	@Autowired
-	public NavigatorController(String uri, MolgenisPluginFactory molgenisPluginFactory)
+	public NavigatorController()
 	{
-		super(uri, molgenisPluginFactory);
+		super(URI);
 	}
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String init(HttpServletRequest request, Model model)
+	public String init()
 	{
-		return "view-gn-app";
+		return "view-navigator";
 	}
 }
