ab67da82c2e3251dc4a45da98519923696ae8362
Mark de Haan <markdehaan90@gmail.com>
2017-05-19 14:36:54 +0200

    Initial repository setup

---
+++ web-app/src/main/java/org/molgenis/app/WebAppInitializer.java
@@ -0,0 +1,16 @@
+package org.molgenis.org.controller.app;
+
+import org.molgenis.ui.MolgenisWebAppInitializer;
+import org.springframework.web.WebApplicationInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+public class WebAppInitializer extends MolgenisWebAppInitializer implements WebApplicationInitializer
+{
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException
+	{
+		super.onStartup(servletContext, WebAppConfig.class, true, 150);
+	}
+}
