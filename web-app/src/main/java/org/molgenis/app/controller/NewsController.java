9c9caf50d91755c62a0a650703d29cfb06aa5536
connorstroomberg <c.stroomberg@gmail.com>
2017-05-22 12:26:08 +0200

    fix package structure

--- web-app/src/main/java/org/molgenis/app/controller/NewsController.java
+++ web-app/src/main/java/org/molgenis/app/controller/NewsController.java
@@ -1,11 +1,11 @@
-package org.molgenis.org.controller.app.controller;
+package org.molgenis.app.controller;
 
 import org.molgenis.ui.MolgenisPluginController;
 import org.molgenis.ui.controller.AbstractStaticContentController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import static org.molgenis.org.controller.app.controller.NewsController.URI;
+import static org.molgenis.app.controller.NewsController.URI;
 
 /**
  * Controller that handles news page requests
