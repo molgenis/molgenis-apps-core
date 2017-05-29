14dcf4b207c142eb4811398208b77d4561ec3e56
Mark-de-Haan <markdehaan90@gmail.com>
2017-05-29 16:53:45 +0200

    MetadataManager setup

---
+++ molgenis-metadata-manager/src/main/java/org/molgenis/metadata/manager/controller/MetadataManagerController.java
@@ -0,0 +1,26 @@
+package org.molgenis.metadata.manager.controller;
+
+import org.molgenis.ui.MolgenisPluginController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import static org.molgenis.metadata.manager.controller.MetadataManagerController.URI;
+
+@Controller
+@RequestMapping(URI)
+public class MetadataManagerController extends MolgenisPluginController
+{
+	public static final String METADATA_MANAGER = "metadata-manager";
+	public static final String URI = PLUGIN_URI_PREFIX + METADATA_MANAGER;
+
+	public MetadataManagerController()
+	{
+		super(URI);
+	}
+
+	@RequestMapping
+	public String init()
+	{
+		return "view-metadata-manager";
+	}
+}
