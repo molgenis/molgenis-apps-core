ecc8b5bbbac2191f8eec0d14dc04922d6ae1c632
Mark-de-Haan <markdehaan90@gmail.com>
2017-05-30 16:25:57 +0200

    Remove docs

--- molgenis-metadata-manager/src/main/frontend/src/store/mutations.js
+++ molgenis-metadata-manager/src/main/frontend/src/store/mutations.js
@@ -6,21 +6,10 @@ export const SET_EDITOR_ENTITY_TYPE = '__SET_EDITOR_ENTITY_TYPE__'
 export const UPDATE_EDITOR_ENTITY_TYPE = '__UPDATE_EDITOR_ENTITY_TYPE__'
 
 export default {
-  /**
-   * Create an active alert with a given message and type
-   *
-   * @param state state of the application
-   * @param alert alert object containing 1. message and 2. type
-   */
   [CREATE_ALERT] (state, alert) {
     state.showAlert = true
     state.alert = alert
   },
-  /**
-   * Remove an active alert and set message and type back to null
-   *
-   * @param state state of the application
-   */
   [REMOVE_ALERT] (state) {
     state.showAlert = false
     state.alert.message = null
