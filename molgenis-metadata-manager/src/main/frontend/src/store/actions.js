1f67a2f0564935ae6b9b78f37efa9b6c9b243d84
Mark-de-Haan <markdehaan90@gmail.com>
2017-05-30 12:38:19 +0200

    Replace vue-resource with molgenisAPI

--- molgenis-metadata-manager/src/main/frontend/src/store/actions.js
+++ molgenis-metadata-manager/src/main/frontend/src/store/actions.js
@@ -1,17 +1,14 @@
-import Vue from 'vue'
-import VueResource from 'vue-resource'
+import { get } from './rest-client/molgenisAPI'
 
 export const GET_ENTITY_TYPES = '__GET_ENTITY_TYPES__'
 export const GET_ENTITY_TYPE_BY_ID = '__GET_ENTITY_TYPE_BY_ID__'
 
-Vue.use(VueResource)
 export default {
   [GET_ENTITY_TYPES] ({commit, state}) {
     console.log('placeholder action to get entity types')
   },
   [GET_ENTITY_TYPE_BY_ID] ({commit, state}, entityTypeID) {
-    Vue.http.headers.common['x-molgenis-token'] = 'test'
-    Vue.http.get('metadata-manager/entityType/' + entityTypeID)
+    get({apiUrl: '/metadata-manager'}, '/entityType/' + entityTypeID)
       .then(response => {
         console.log('success', response)
       }, response => {
