6e43eb59874a4dc5c1c7ece3c24c7fdacb8ed592
connorstroomberg <c.stroomberg@gmail.com>
2017-06-08 17:26:40 +0200

    place module js build into named subfolders WIP

--- navigator/src/main/frontend/build/webpack.prod.conf.js
+++ navigator/src/main/frontend/build/webpack.prod.conf.js
@@ -20,8 +20,8 @@ var webpackConfig = merge(baseWebpackConfig, {
   devtool: config.build.productionSourceMap ? '#source-map' : false,
   output: {
     path: config.build.assetsRoot,
-    filename: utils.assetsPath('js/[name].js'),
-    chunkFilename: utils.assetsPath('js/[id].js')
+    filename: utils.assetsPath('js/navigator/[name].js'),
+    chunkFilename: utils.assetsPath('js/navigator/[id].js')
   },
   plugins: [
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
@@ -35,7 +35,7 @@ var webpackConfig = merge(baseWebpackConfig, {
     }),
     // extract css into its own file
     new ExtractTextPlugin({
-      filename: utils.assetsPath('css/[name].css')
+      filename: utils.assetsPath('css/navigator/[name].css')
     }),
     // generate dist index.html with correct asset hash for caching.
     // you can customize output by editing /index.html
