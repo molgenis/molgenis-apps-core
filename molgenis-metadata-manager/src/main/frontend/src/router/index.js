16ecf1c85f26440cd36555cc2eaa40b2241e8eda
Mark-de-Haan <markdehaan90@gmail.com>
2017-06-12 16:13:00 +0200

    export INITIAL_STATE object from state.js to fix tests

--- molgenis-metadata-manager/src/main/frontend/src/router/index.js
+++ molgenis-metadata-manager/src/main/frontend/src/router/index.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import MetadataManagerContainer from 'components/MetadataManagerContainer'
+import { INITIAL_STATE } from '../store/state'
+
 Vue.use(Router)
 export default new Router({
   mode: 'history',
   // Set base URL which is provided by the server
   // e.g. metadata-manager base URL is /menu/main/metadata-manager
-  base: window.__INITIAL_STATE__.baseUrl,
+  base: INITIAL_STATE.baseUrl,
   routes: [
     {
       path: '/',
--- navigator/src/main/frontend/src/router/index.js
+++ navigator/src/main/frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Navigator from 'components/Navigator'
+import { INITIAL_STATE } from '../store/state'
 
 Vue.use(Router)
 
@@ -8,7 +9,7 @@ export default new Router({
   mode: 'history',
   // Set base URL which is provided by the server
   // e.g. navigator base URL is /menu/main/navigator
-  base: window.__INITIAL_STATE__.baseUrl,
+  base: INITIAL_STATE.baseUrl,
   routes: [
     {
       path: '/',
