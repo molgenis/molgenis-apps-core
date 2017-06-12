117e0586a2ba6ccb0f03fd341536e570e3428aea
Mark-de-Haan <markdehaan90@gmail.com>
2017-06-12 13:44:51 +0200

    Added junit reporting to karma config

--- molgenis-metadata-manager/src/main/frontend/test/unit/karma.conf.js
+++ molgenis-metadata-manager/src/main/frontend/test/unit/karma.conf.js
@@ -17,10 +17,15 @@ module.exports = function (config) {
       noInfo: true
     },
     coverageReporter: {
-      dir: './coverage',
+      dir: '../../../../../target/generated-sources/coverage',
       reporters: [
-        { type: 'cobertura', subdir: 'cobertura' }
+        {type: 'cobertura', subdir: 'cobertura'}
       ]
+    },
+    junitReporter: {
+      outputDir: '../../../../../target/surefire-reports',
+      outputFile: 'TEST-results.xml',
+      useBrowserName: false
     }
   })
 }
